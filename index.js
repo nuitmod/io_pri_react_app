d57540a4897ffbb68505aa164d7f6048152beb20
nuitmod <wtvfqm@gmail.com>
2020-12-04 17:25:12 +0500

    add songs

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 //import './news-article.js';
 //import { topHeadlinesUrl } from './newsApi.js';
-
-window.addEventListener('load', () => {
-  registerSW();
-});
-/*
-async function getNews() {
-  const res = await fetch(topHeadlinesUrl);
-  const json = await res.json();
-
-  const main = document.querySelector('main');
-
-}
-*/
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('./sw.js');
-    } catch (e) {
-      console.log(`SW registration failed`);
-    }
-  }
-}
+//
+// window.addEventListener('load', () => {
+//   registerSW();
+// });
+// /*
+// async function getNews() {
+//   const res = await fetch(topHeadlinesUrl);
+//   const json = await res.json();
+//
+//   const main = document.querySelector('main');
+//
+// }
+// */
+// async function registerSW() {
+//   if ('serviceWorker' in navigator) {
+//     try {
+//       await navigator.serviceWorker.register('./sw.js');
+//     } catch (e) {
+//       console.log(`SW registration failed`);
+//     }
+//   }
+// }
 
 $('.mobile-tab').hide();
 
